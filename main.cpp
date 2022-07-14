73deba2bce141abc79af23da7227f6ff263d7368
prudn1k0va <aristvodolaz@mail.ru>
2022-07-14 17:34:05 +0300

    2 com

--- main.cpp
+++ main.cpp
@@ -37,5 +37,52 @@ void print(vector<float>x, int n) {
 int main()
 {
 
+    float e, delta, delta1, k;
+    int m = 1;
+    vector <float> x0(2), x1(2), x2(2), x3(2);
+
+    cout << "Enter x1" << endl;
+    cin >> x0[0];
+    cout << "Enter x2" << endl;
+    cin >> x0[1];
+    cout << "Enter accuracy" << endl;
+    cin >> e;
+    cout << "Enter delta" << endl;
+    cin >> delta;
+    k = 1;
+
+    do {
+        delta1 = delta;
+        do {
+            x1 = x0;
+            for (int i = 0; i < 2; i++) {
+                if (f_delta(x1, delta, i) < f(x1))
+                    x1[i] = x1[i] + delta;
+                else
+                    if (f_delta(x1, -delta, i) < f(x1))
+                        x1[i] = x1[i] - delta;
+            }
+
+            if (x1 == x0)
+                delta = delta / 2;
+        } while ((x1 == x0) && (delta >= e));
+
+        x2 = x0;
+        x3 = x1;
+
+        while (f(x3) < f(x2)) {
+            x2 = x3;
+            x3 = sum(x2, mul(dif(x1, x0, 2), k, 2), 2);
+        }
+
+        x0 = x2;
+        cout << "i=" << m << endl;
+        cout << "x=" << x2[0] << ' ' << x2[1] << endl;
+        cout << "delta=" << delta1 << endl;
+        m++;
+    } while (delta1 > e);
+
+    cout << endl;
+    print(x0, 2);
     return 0;
 }
